1583e19de902860266bb23bd5f0fa204d3ecae41
Tom Chauvel <tom.chauvel@hotmail.com>
2023-01-01 22:57:12 +0100

    update

--- main.js
+++ main.js
@@ -2,28 +2,24 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
   const win = new BrowserWindow({
-    frame: false,
+    frame: true,
     width: 1000,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
+    height: 620,
+    
   })
+  win.setMenu(null)
   win.loadFile('src/index.html')
-  //win.webContents.openDevTools()
 }
 
-app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  app.quit()
 })
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
+app.whenReady().then(() => {
+  createWindow()
 
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
